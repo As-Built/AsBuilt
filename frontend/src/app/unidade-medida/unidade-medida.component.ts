d577a551dc7def3d81a75a88e846a8e515e088ff
Rafael Kampa <rafael_kampa@hotmail.com>
2024-05-21 00:08:48 -0300

    Front para cadastro e consulta de Unidade de Medida

--- frontend/src/app/unidade-medida/unidade-medida.component.ts
+++ frontend/src/app/unidade-medida/unidade-medida.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { UnidadeMedidaModel } from '../shared/model/unidade-medida.model';
+import { UnidadeMedidaService } from './service/unidade-medida.service';
+import { catchError, firstValueFrom, of, tap } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-unidade-medida',
   templateUrl: './unidade-medida.component.html',
   styleUrls: ['./unidade-medida.component.scss']
 })
-export class UnidadeMedidaComponent {
+export class UnidadeMedidaComponent implements OnInit {
+
+  @ViewChild('modalEditarUnidade', { static: true })
+  modalEditarUnidade!: ElementRef;
+
+  unidadeDeMedidaModel = new UnidadeMedidaModel();
+  cadastroUnidadeDeMedida = new UnidadeMedidaModel();
+  listaUnidadeDeMedida: UnidadeMedidaModel[] = [];
+  displayedColumns: string[] = ["name", 'description'];
+  renderModalVisualizar = false;
+  indDesabilitaCampos = true;
+  isCadastro = true;
+
+  constructor(
+    private unidadeDeMedidaService: UnidadeMedidaService
+  ) { }
+
+  ngOnInit(): void {
+    this.buscarUnidadesDeMedida();
+  }
+
+
+  async buscarUnidadesDeMedida() {
+    try {
+      const centrosDeCusto: any = await firstValueFrom(this.unidadeDeMedidaService.listarUnidadesDeMedida());
+      this.listaUnidadeDeMedida = centrosDeCusto;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  mudarAba() {
+    this.buscarUnidadesDeMedida();
+    this.isCadastro = !this.isCadastro;
+    this.cadastroUnidadeDeMedida = new UnidadeMedidaModel();
+  }
+
+  cadastrarUnidadeDeMedida() {
+    this.validarCampos(this.cadastroUnidadeDeMedida);
+    this.unidadeDeMedidaService.cadastrarUnidadesDeMedida(this.cadastroUnidadeDeMedida).pipe(
+      tap(retorno => {
+        Swal.fire({
+          text: "Cadastro realizado com sucesso!",
+          icon: "success",
+          showConfirmButton: false,
+          timer: 2000
+        });
+        this.cadastroUnidadeDeMedida = new UnidadeMedidaModel();
+      }),
+      catchError(error => {
+        if (error.error == "Unit Measurement already exists") {
+          Swal.fire({
+            text: "Já existe uma unidade de medida com essa sigla!",
+            icon: "error",
+            showConfirmButton: false,
+            timer: 2000
+          });
+        }
+        return of();
+      })
+    ).subscribe();
+  }
+
+  
+  validarCampos(unidadeDeMedida: UnidadeMedidaModel) {
+    if (unidadeDeMedida.name === null || unidadeDeMedida.name.trim() === ""
+      || unidadeDeMedida.name === undefined) {
+      Swal.fire({
+        text: "O campo 'Sigla' é obrigatório!",
+        icon: "warning",
+        showConfirmButton: false,
+        timer: 2000
+      });
+      return;
+    }
+    if (unidadeDeMedida.description === null || unidadeDeMedida.description.trim() === ""
+      || unidadeDeMedida.description === undefined) {
+      Swal.fire({
+        text: "O campo 'Descrição' é obrigatório!",
+        icon: "warning",
+        showConfirmButton: false,
+        timer: 2000
+      });
+      return;
+    }
+  }
 
 }
