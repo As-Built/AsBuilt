2fe8b8adc52891368173404168c8adfa523f8403
Rafael Kampa <rafael_kampa@hotmail.com>
2024-08-24 20:52:38 -0300

    Remoção do perfil de usuário do sidebar

--- frontend/src/app/shared/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { jwtDecode } from "jwt-decode";
+import { Component, Input } from '@angular/core';
 
 
 @Component({
@@ -7,31 +6,12 @@ import { jwtDecode } from "jwt-decode";
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit{
-
-
-  ngOnInit(): void {
-    this.userName = this.getUserName();
-  }
-
-  public userName: string = '';
+export class SidebarComponent {
 
   @Input ()
   isSidebarOpen = true;
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
-  }
-
-  getUserName(): string {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      return "";
-    }
-  
-    const decodedToken = jwtDecode(token) as any;
-    const fullName = decodedToken.user.name;
-    const firstName = fullName.split(' ')[0];
-    return firstName;
   }
 }
