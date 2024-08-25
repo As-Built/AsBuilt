2e5916ddd9213eba62d0e1326da52b5728846c94
Rafael Kampa <rafael_kampa@hotmail.com>
2024-08-24 21:35:25 -0300

    Correções no sidebar e scroll de página ao mudar a rota

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
@@ -7,10 +7,19 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   constructor(private router: Router) { }
-
+  
+  ngOnInit() {
+    // Seta o scroll para o topo da página ao mudar de rota
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
+    });
+  }
 
   public get mostrarSidebar(): boolean {
     const url = this.router.url;
